6cb47c6bd4a150f1889d4aed47b416242ef84044
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-02-28 16:34:40 -0500

    initial setup

---
+++ src/PassageIdentity/Client.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PassageIdentity
+{
+    public class Client
+    {
+
+    }
+}
---
+++ tests/PassageIdentity.Tests/ClientTests.cs
@@ -0,0 +1,11 @@
+namespace PassageIdentity.Tests
+{
+    public class ClientTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
