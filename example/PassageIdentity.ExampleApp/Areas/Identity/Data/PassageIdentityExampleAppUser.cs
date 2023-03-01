1a4b185676f775abd9e1f54f008f1eb041d2675c
Kori Francis <byter@me.com>
2023-03-01 16:51:48 -0500

    wip elements

---
+++ example/PassageIdentity.ExampleApp/Areas/Identity/Data/PassageIdentityExampleAppUser.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PassageIdentity.ExampleApp.Areas.Identity.Data;
+
--- src/PassageIdentity/Config.cs
+++ src/PassageIdentity/Config.cs
@@ -9,7 +9,9 @@ namespace PassageIdentity
     public interface IPassageConfig
     {
         string? ApiKey { get; set; }
+
         string? AppId { get; set; }
+
         PassageAuthStrategy AuthStrategy { get; set; }
 
         string? PublicKey { get; set; }
@@ -27,8 +29,19 @@ namespace PassageIdentity
         }
 
         public string? ApiKey { get; set; }
+
         public string? AppId { get; set; }
+
         public PassageAuthStrategy AuthStrategy { get; set; } = PassageAuthStrategy.Cookie;
+
+        /// <summary>
+        /// <para>This is your public key Base64-encoded. It is in PEM format</para>
+        /// <para>
+        /// Your web application must use this key to verify the JWTs created by Passage on behalf of your users.
+        /// For more information and sample code, see our docs on https://docs.passage.id/advanced/security
+        /// </para>
+        /// <para>This key is specific to this application and can only be used to verify users for this application.</para>
+        /// </summary>
         public string? PublicKey { get; set; }
     }
 }
