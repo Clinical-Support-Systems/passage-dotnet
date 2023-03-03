d04806d45a2c10cb547290fa46b375078f96d201
Victoria Tolls <vtolls@clinicalsupportsystems.com>
2023-03-03 11:10:23 -0500

    wip - managements api calls and tests

--- tests/PassageIdentity.Tests/Intergration/ManagementIntegtrationTests.cs
+++ tests/PassageIdentity.Tests/Intergration/ManagementIntegtrationTests.cs
@@ -28,4 +28,75 @@ public class ManagementIntegtrationTests : IntegrationTestBase
             x => x!.Id.ShouldBe(PassageConfig.AppId)
         );
     }
+
+    [Fact]
+    public async Task Can_Get_Apps()
+    {
+        // Arrange
+        var logger = Substitute.For<ILogger>();
+        var client = new PassageClient(logger, HttpClientFactory, PassageConfig);
+
+        // Act
+        var app = await client.Management.GetAppsAsync().ConfigureAwait(false);
+
+        // Assert
+        app.ShouldSatisfyAllConditions(
+            x => x.ShouldNotBeNull(),
+            x => x!.Any().ShouldNotBe(false)
+        );
+    }
+
+    [Fact]
+    public async Task Can_Get_Users()
+    {
+        // Arrange
+        var logger = Substitute.For<ILogger>();
+        var client = new PassageClient(logger, HttpClientFactory, PassageConfig);
+
+        // Act
+        var app = await client.Management.GetUsersAsync().ConfigureAwait(false);
+
+        // Assert
+        app.ShouldSatisfyAllConditions(
+            x => x.ShouldNotBeNull(),
+            x => x!.Any().ShouldNotBe(false)
+        );
+    }
+
+    [Fact]
+    public async Task Can_Get_User()
+    {
+        // Arrange
+        var logger = Substitute.For<ILogger>();
+        var client = new PassageClient(logger, HttpClientFactory, PassageConfig);
+
+        var identifier = "victoria@clinicalsupportsystems.com";
+
+        // Act
+        var app = await client.Management.GetUserAsync(identifier).ConfigureAwait(false);
+
+        // Assert
+        app.ShouldSatisfyAllConditions(
+            x => x.ShouldNotBeNull(),
+            x => x.Id.ShouldBe(identifier)
+        );
+    }
+
+    [Fact]
+    public async Task Can_Create_App()
+    {
+        // Arrange
+        var logger = Substitute.For<ILogger>();
+        var client = new PassageClient(logger, HttpClientFactory, PassageConfig);
+
+        var testApp = new App() { AllowedIdentifier = AllowedIdentifier.Email, AuthOrigin = new Uri("https://localhost:7294"), Name = "TestCreateApp", PublicSignup = false, RedirectUrl = new Uri("https://localhost:7294/signin-passage") };
+
+        // Act
+        var app = await client.Management.CreateAppAsync(testApp).ConfigureAwait(false);
+
+        // Assert
+        app.ShouldSatisfyAllConditions(
+            x => x.ShouldNotBeNull()
+        );
+    }
 }
