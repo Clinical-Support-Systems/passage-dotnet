64dbb74e22fb26d2a17fd6e3aec1e2e6ff879692
Kori Francis <byter@me.com>
2023-03-08 13:23:26 -0500

    add passage-auth event handler properties

--- src/PassageIdentity.TagHelpers/PassageAuthTagHelper.cs
+++ src/PassageIdentity.TagHelpers/PassageAuthTagHelper.cs
@@ -15,6 +15,18 @@ namespace PassageIdentity.TagHelpers
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; } = null!;
 
+        /// <summary>
+        /// Javascript function name. The user's identifier is provided as a parameter.
+        /// After doing additional work to validate any other requirements, the callback
+        /// should return true to continue with the registration/login, or return
+        /// false to cancel the registration/login.
+        /// </summary>
+        [HtmlAttributeName("on-before-auth")]
+        public string? BeforeAuth { get; set; }
+
+        [HtmlAttributeName("on-success")]
+        public string? OnSuccess { get; set; }
+
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (output is null)
@@ -23,6 +35,18 @@ namespace PassageIdentity.TagHelpers
             }
 
             output.TagName = "passage-auth";
+
+            if (!string.IsNullOrEmpty(BeforeAuth))
+            {
+                var script = $"<script>document.querySelector('passage-auth').beforeAuth = function(arg){{ {BeforeAuth}(arg);}};</script>";
+                output.PostElement.AppendHtml(script);
+            }
+
+            if (!string.IsNullOrEmpty(OnSuccess))
+            {
+                var script = $"<script>document.querySelector('passage-auth').onSuccess = function(arg){{ {OnSuccess}(arg); }};</script>";
+                output.PostElement.AppendHtml(script);
+            }
 
             return base.ProcessAsync(context, output);
         }
