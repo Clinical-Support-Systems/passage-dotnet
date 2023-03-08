83828de5907ec49dbb6d45c0a06040ce2c8567a7
Kori Francis <byter@me.com>
2023-03-08 13:25:02 -0500

    apply to all element taghelpers

--- src/PassageIdentity.TagHelpers/PassageBaseTagHelper.cs
+++ src/PassageIdentity.TagHelpers/PassageBaseTagHelper.cs
@@ -16,6 +16,18 @@ namespace PassageIdentity.TagHelpers
         [HtmlAttributeName("default-country-code")]
         public string? DefaultCountryCode { get; set; }
 
+        /// <summary>
+        /// Javascript function name. The user's identifier is provided as a parameter.
+        /// After doing additional work to validate any other requirements, the callback
+        /// should return true to continue with the registration/login, or return
+        /// false to cancel the registration/login.
+        /// </summary>
+        [HtmlAttributeName("on-before-auth")]
+        public string? BeforeAuth { get; set; }
+
+        [HtmlAttributeName("on-success")]
+        public string? OnSuccess { get; set; }
+
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (context is null)
@@ -33,6 +45,18 @@ namespace PassageIdentity.TagHelpers
 
             if (!string.IsNullOrEmpty(AppId)) output.Attributes.Add(new TagHelperAttribute("app-id", AppId));
             if (!string.IsNullOrEmpty(DefaultCountryCode)) output.Attributes.Add(new TagHelperAttribute("default-country-code", DefaultCountryCode));
+
+            if (!string.IsNullOrEmpty(BeforeAuth))
+            {
+                var beforeAuthScript = $"<script>document.querySelector('passage-auth').beforeAuth = function(arg){{ {BeforeAuth}(arg);}};</script>";
+                output.PostElement.AppendHtml(beforeAuthScript);
+            }
+
+            if (!string.IsNullOrEmpty(OnSuccess))
+            {
+                var onSuccessScript = $"<script>document.querySelector('passage-auth').onSuccess = function(arg){{ {OnSuccess}(arg); }};</script>";
+                output.PostElement.AppendHtml(onSuccessScript);
+            }
 
             return base.ProcessAsync(context, output);
         }
